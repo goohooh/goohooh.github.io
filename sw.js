2b0b2a8dd58bcaaa3019359cc0ea8a9089fddeb0
goohooh <goo_oo@naver.com>
2022-01-01 14:13:38 +0900

    Updates

--- sw.js
+++ sw.js
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-9252bfa5ea71b588207d.js"
   },
   {
-    "url": "app-53a306aceabb6977fc78.js"
+    "url": "app-cdbcf2f30650513ac2d2.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-15096bed533ed0ff8b58.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1867e93d0115a561b24e188664fac2aa"
+    "revision": "031322f0610453a78bb72b50201c9154"
   },
   {
     "url": "polyfill-c5f50f2ad13861331c8a.js"
@@ -76,7 +76,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-53a306aceabb6977fc78.js`))) {
+  if (!resources || !(await caches.match(`/app-cdbcf2f30650513ac2d2.js`))) {
     return await fetch(event.request)
   }
 
